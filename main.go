4e214f29bd62f60b6bd1893ecfca58d85c79ba1e
Chris Czub <chris.czub@gmail.com>
2016-08-03 23:43:12 -0400

    try this again

--- main.go
+++ main.go
@@ -532,6 +532,7 @@ func triggerBehavior(behavior GPIOBehavior, context *ScreenContext) {
 	}
 
 	if behavior.Behavior == "toggleTrack" {
+		log.Println("toggling %d\n", behavior.Pin)
 		toggleVolume(behavior.Pin, context)
 	}
 
@@ -664,7 +665,7 @@ func main() {
 		// when the phone is put back down, wait for it to be lifted again and repeat
 
 		// check every 1/8 second
-		// timeToCheck := time.Millisecond * 125
+		timeToCheck := time.Millisecond * 125
 
 		// hacking this in here because i don't have time to do it well
 		// combos := [][]int{{4, 27}, {4, 6}, {4, 19}, {17, 27}, {17, 6}, {17, 19}, {27, 5}, {22, 13}, {22, 19}, {5, 6}, {5, 19}, {26, 18}, {12, 16}}
@@ -674,9 +675,8 @@ func main() {
 			comboPins[i] = []rpio.Pin{rpio.Pin(pair[0]), rpio.Pin(pair[1])}
 		}
 
-		// ticker := time.NewTicker(timeToCheck)
-		// for _ = range ticker.C {
-		for {
+		ticker := time.NewTicker(timeToCheck)
+		for _ = range ticker.C {
 			for i, pair := range comboPins {
 				// we need to fire some electrons through pair[0]
 				// and receive at pair[1]
